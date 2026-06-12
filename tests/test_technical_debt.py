4e1b12500c0237969f1faab6d46b9ec5426648d0
mohammedshihaf10 <mohammedshihaf10@users.noreply.github.com>
2026-06-12 15:32:35 +0530

    Add metric-scoped SA SA_TDI_BugFX_2.6 codebase

---
+++ tests/test_technical_debt.py
@@ -0,0 +1,111 @@
+from __future__ import print_function
+import unittest
+class TestTDIFull(unittest.TestCase):
+ def test_technical_debt_0(self):
+  from sa.technical_debt import debt_calculator_b0_v0
+  self.assertTrue(debt_calculator_b0_v0(100, 0.1, 2))
+ def test_technical_debt_1(self):
+  from sa.technical_debt import debt_calculator_b1_v0
+  self.assertTrue(debt_calculator_b1_v0(100, 0.1, 2))
+ def test_technical_debt_2(self):
+  from sa.technical_debt import debt_calculator_b2_v0
+  self.assertTrue(debt_calculator_b2_v0(100, 0.1, 2))
+ def test_technical_debt_3(self):
+  from sa.technical_debt import debt_calculator_b3_v0
+  self.assertTrue(debt_calculator_b3_v0(100, 0.1, 2))
+ def test_technical_debt_4(self):
+  from sa.technical_debt import debt_calculator_b4_v0
+  self.assertTrue(debt_calculator_b4_v0(100, 0.1, 2))
+ def test_technical_debt_5(self):
+  from sa.technical_debt import debt_calculator_b5_v0
+  self.assertTrue(debt_calculator_b5_v0(100, 0.1, 2))
+ def test_technical_debt_6(self):
+  from sa.technical_debt import debt_calculator_b6_v0
+  self.assertTrue(debt_calculator_b6_v0(100, 0.1, 2))
+ def test_technical_debt_7(self):
+  from sa.technical_debt import debt_calculator_b7_v0
+  self.assertTrue(debt_calculator_b7_v0(100, 0.1, 2))
+ def test_technical_debt_8(self):
+  from sa.technical_debt import debt_calculator_b8_v0
+  self.assertTrue(debt_calculator_b8_v0(100, 0.1, 2))
+ def test_technical_debt_9(self):
+  from sa.technical_debt import debt_calculator_b9_v0
+  self.assertTrue(debt_calculator_b9_v0(100, 0.1, 2))
+ def test_technical_debt_10(self):
+  from sa.technical_debt import debt_calculator_b10_v0
+  self.assertTrue(debt_calculator_b10_v0(100, 0.1, 2))
+ def test_technical_debt_11(self):
+  from sa.technical_debt import debt_calculator_b11_v0
+  self.assertTrue(debt_calculator_b11_v0(100, 0.1, 2))
+ def test_technical_debt_12(self):
+  from sa.technical_debt import debt_calculator_b12_v0
+  self.assertTrue(debt_calculator_b12_v0(100, 0.1, 2))
+ def test_technical_debt_13(self):
+  from sa.technical_debt import debt_calculator_b13_v0
+  self.assertTrue(debt_calculator_b13_v0(100, 0.1, 2))
+ def test_technical_debt_14(self):
+  from sa.technical_debt import debt_calculator_b14_v0
+  self.assertTrue(debt_calculator_b14_v0(100, 0.1, 2))
+ def test_technical_debt_15(self):
+  from sa.technical_debt import debt_calculator_b15_v0
+  self.assertTrue(debt_calculator_b15_v0(100, 0.1, 2))
+ def test_technical_debt_16(self):
+  from sa.technical_debt import debt_calculator_b16_v0
+  self.assertTrue(debt_calculator_b16_v0(100, 0.1, 2))
+ def test_technical_debt_17(self):
+  from sa.technical_debt import debt_calculator_b17_v0
+  self.assertTrue(debt_calculator_b17_v0(100, 0.1, 2))
+ def test_technical_debt_18(self):
+  from sa.technical_debt import debt_calculator_b18_v0
+  self.assertTrue(debt_calculator_b18_v0(100, 0.1, 2))
+ def test_technical_debt_19(self):
+  from sa.technical_debt import debt_calculator_b19_v0
+  self.assertTrue(debt_calculator_b19_v0(100, 0.1, 2))
+ def test_technical_debt_20(self):
+  from sa.technical_debt import debt_calculator_b20_v0
+  self.assertTrue(debt_calculator_b20_v0(100, 0.1, 2))
+ def test_technical_debt_21(self):
+  from sa.technical_debt import debt_calculator_b21_v0
+  self.assertTrue(debt_calculator_b21_v0(100, 0.1, 2))
+ def test_technical_debt_22(self):
+  from sa.technical_debt import debt_calculator_b22_v0
+  self.assertTrue(debt_calculator_b22_v0(100, 0.1, 2))
+ def test_technical_debt_23(self):
+  from sa.technical_debt import debt_calculator_b23_v0
+  self.assertTrue(debt_calculator_b23_v0(100, 0.1, 2))
+ def test_technical_debt_24(self):
+  from sa.technical_debt import debt_calculator_b24_v0
+  self.assertTrue(debt_calculator_b24_v0(100, 0.1, 2))
+ def test_technical_debt_25(self):
+  from sa.technical_debt import debt_calculator_b25_v0
+  self.assertTrue(debt_calculator_b25_v0(100, 0.1, 2))
+ def test_technical_debt_26(self):
+  from sa.technical_debt import debt_calculator_b26_v0
+  self.assertTrue(debt_calculator_b26_v0(100, 0.1, 2))
+ def test_technical_debt_27(self):
+  from sa.technical_debt import debt_calculator_b27_v0
+  self.assertTrue(debt_calculator_b27_v0(100, 0.1, 2))
+ def test_technical_debt_28(self):
+  from sa.technical_debt import debt_calculator_b28_v0
+  self.assertTrue(debt_calculator_b28_v0(100, 0.1, 2))
+ def test_technical_debt_29(self):
+  from sa.technical_debt import debt_calculator_b29_v0
+  self.assertTrue(debt_calculator_b29_v0(100, 0.1, 2))
+ def test_technical_debt_30(self):
+  from sa.technical_debt import debt_calculator_b30_v0
+  self.assertTrue(debt_calculator_b30_v0(100, 0.1, 2))
+ def test_technical_debt_31(self):
+  from sa.technical_debt import debt_calculator_b31_v0
+  self.assertTrue(debt_calculator_b31_v0(100, 0.1, 2))
+ def test_technical_debt_32(self):
+  from sa.technical_debt import debt_calculator_b32_v0
+  self.assertTrue(debt_calculator_b32_v0(100, 0.1, 2))
+ def test_technical_debt_33(self):
+  from sa.technical_debt import debt_calculator_b33_v0
+  self.assertTrue(debt_calculator_b33_v0(100, 0.1, 2))
+ def test_technical_debt_34(self):
+  from sa.technical_debt import debt_calculator_b34_v0
+  self.assertTrue(debt_calculator_b34_v0(100, 0.1, 2))
+ def test_technical_debt_35(self):
+  from sa.technical_debt import debt_calculator_b35_v0
+  self.assertTrue(debt_calculator_b35_v0(100, 0.1, 2))
